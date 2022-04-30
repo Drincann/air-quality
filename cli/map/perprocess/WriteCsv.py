2b6e3f88f7151c22071130145d238975257aabb5
CYHei_mu <CYHei_mu@163.com>
2022-04-30 21:03:57 +0800

    Update WriteCsv.py with numpy

--- cli/map/perprocess/WriteCsv.py
+++ cli/map/perprocess/WriteCsv.py
@@ -1,18 +1,18 @@
-import csv
+import numpy as np
+import time
 
 # 创建csv文件
 # 参数：经度数组，纬度数组，数据数组，输出路径，输出文件名
 # 返回：无
 
-
 def run(lon, lat, data, outpath, name):
     print("开始写入文件" + name)
 
-    csvfile = open(outpath + "/" + name, 'w', newline="")
-    writer = csv.writer(csvfile)
-    writer.writerow(['lon', 'lat', 'data'])
-    for i in range(len(data)):
-        writer.writerow(
-            [round(lon[i], 2), round(lat[i], 2), round(data[i], 2)])
+    tic = time.time()
 
-    print("文件" + name + "写入完成")
+    res = np.array([lon, lat, data])
+    np.savetxt(outpath + "/" + name, res.T, delimiter=",", fmt='%.2f')
+    
+    toc = time.time()
+
+    print("文件" + name + "写入完成，用时" + str(int(toc - tic)) + "s")
