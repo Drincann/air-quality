3e631284d8fd31ba86c3f07fc43261e757660d2e
Drincann <1019933576@qq.com>
2022-05-01 03:22:10 +0800

    fix: 自动创建文件夹, 并为输出的 csv 添加表头

--- cli/map/perprocess/WriteCsv.py
+++ cli/map/perprocess/WriteCsv.py
@@ -1,9 +1,10 @@
 import numpy as np
 import time
-
+import os
 # 创建csv文件
 # 参数：经度数组，纬度数组，数据数组，输出路径，输出文件名
 # 返回：无
+
 
 def run(lon, lat, data, outpath, name):
     print("开始写入文件" + name)
@@ -11,8 +12,17 @@ def run(lon, lat, data, outpath, name):
     tic = time.time()
 
     res = np.array([lon, lat, data])
-    np.savetxt(outpath + "/" + name, res.T, delimiter=",", fmt='%.2f')
-    
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+    np.savetxt(os.path.join(outpath, name), res.T, delimiter=",", fmt='%.2f')
+
+    # 头插一行
+    with open(os.path.join(outpath, name), 'r') as f:
+        lines = f.readlines()
+    with open(os.path.join(outpath, name), 'w') as f:
+        f.write('lon,lat,data\n')
+        f.writelines(lines)
+
     toc = time.time()
 
     print("文件" + name + "写入完成，用时" + str(int(toc - tic)) + "s")
